3080da9757cab1418106cfcf3e1cf6de249acf56
Mmaryna68 <maryov688@gmail.com>
2023-06-03 03:12:12 +0100

    added new styles and js

--- script.js
+++ script.js
@@ -31,5 +31,40 @@ function fetchPosts() {
     });
 }
 
+function createPost(event) {
+  event.preventDefault();
+
+  const titleInput = document.getElementById("post-title");
+  const bodyInput = document.getElementById("post-body");
+
+  const newPost = {
+    title: titleInput.value,
+    body: bodyInput.value,
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((post) => {
+      const postContainer = document.getElementById("post-container");
+      const postHTML = createPostHTML(post);
+      addPostToContainer(postContainer, postHTML);
+      titleInput.value = "";
+      bodyInput.value = "";
+    })
+    .catch((error) => {
+      console.error("Ошибка при создании поста:", error);
+    });
+}
+
 // Вызываем функцию fetchPosts для начала процесса получения и отображения постов
 fetchPosts();
+
+// Добавляем обработчик события submit для формы, чтобы вызвать функцию createPost при отправке формы
+const postForm = document.getElementById("post-form");
+postForm.addEventListener("submit", createPost);
